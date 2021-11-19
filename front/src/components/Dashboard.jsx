3eb31ba210965206513943489213057206e0a096
Matthew Winne <matt.winne@gmail.com>
2021-11-19 13:31:08 -0800

    Added react, webpack, rest_framework, and linting

---
+++ front/src/components/Dashboard.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+const Dashboard = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchHelloWorld = async () => {
+      const url = "/api/hello_world/";
+      const options = {
+        method: "GET",
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      };
+      const response = await fetch(url, options);
+      // Try catch because .json will throw if no data
+      try {
+        // This will extract the data and convert from a json string to an object
+        const responseData = await response.json();
+        if (response.ok) {
+          setData(responseData.message);
+        } else {
+          setError(responseData);
+        }
+      } catch (e) {
+        setError(e);
+      }
+    };
+    fetchHelloWorld();
+  }, []);0
+
+  // If there is an error show that otherwise the data
+  const displayResults = () => (
+    <div>{error ? `Error: ${error}` : `Data: ${data}`}</div>
+  );
+
+  return (
+    <div>
+      Hello from the dashboard.
+      <br />
+      {displayResults()}
+    </div>
+  );
+};
+
+export default Dashboard;
