08881c4a560b432c0e420e60325bc3e07c3bdc82
Matthew Winne <93726628+mattwinne@users.noreply.github.com>
2021-12-06 22:38:31 -0800

    Adding comments to explain code and needed improvements (#10)
    * Adding comments to explain code and needed improvements
    
    * Removed templates, linted

--- front/src/components/Detail.jsx
+++ front/src/components/Detail.jsx
@@ -1,83 +1,98 @@
-import React, {useState} from "react";
-import { Link,
-        useLocation
-} from 'react-router-dom';
-import useQuestions from "components/Questions"
-import useChoices from "components/Choices"
+import { Link, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import useChoices from "components/Choices";
+import useQuestions from "components/Questions";
 
 const Detail = () => {
-    const location = useLocation()
-    const pathID = location.pathname.slice(-1);
-    const question = useQuestions().filter((q)=>{
-        return q.id == pathID
+  // Pulls the slug from the current URL which represents the question ID. Assigns it to pathID
+  const location = useLocation();
+  const pathID = location.pathname.slice(-1);
+
+  // useQuestions is a custom hook from another component that returns all questions in the database.
+  // Utilizes pathID to identify which question we're on.
+  // Would prefer that it returned the value as a string, but instead it is a single item array
+  const question = useQuestions().filter((q) => {
+    return q.id === pathID;
+  });
+
+  // useChoices is a custom hook form another component that returns all choices in the database.
+  // Utilizes pathID to derive the set of choices associated with the question.
+  const choices = useChoices().filter((c) => {
+    return c.question === pathID;
+  });
+
+  const [radio, setRadio] = useState([]);
+
+  // Initiates on button click.
+  const updateVote = () => {
+    // newData collects the selected choice ID and current votes from the radio button state.
+
+    const newData = {
+      // Since the value of the choice radio buttons were assinged as a string, e.g.(1,3) the values were
+      // assigned by pulling out the individual characters and changing them to an integer.
+      id: parseInt(radio[0], 10),
+      // Increased vote count by one to account for user submission
+      votes: parseInt(radio[2], 10) + 1,
+    };
+    // Utilized PATCH method because I am only updating the vote count, and not the other variables in the object array.
+    fetch(`/api/choices/${radio[0]}/`, {
+      method: "PATCH",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newData),
+    }).then((result) => {
+      result.json().then(() => {
+        // Redirects to results.
+        window.location.replace(`/results/${pathID}`);
+      });
     });
-    const choices = useChoices().filter((c)=>{
-        return c.question == pathID
-    });
+  };
 
-    const [radio, setRadio] = useState([])
+  // TODO: Needs to prevent vote action if no choice is selected
+  return (
+    <>
+      {question.map((item) => (
+        <h1 style={{ color: "blue", fontSize: "18px" }}>{item.text}</h1>
+      ))}
+      {question.length === 0 && (
+        <h2 style={{ color: "black", fontSize: "10px" }}>
+          {" "}
+          ...loading questions
+        </h2>
+      )}
 
-    function updateVote() {
-        let newData = {
-            "id": parseInt(radio[0]),
-            "votes":(parseInt(radio[2]) + 1)
-        }
+      {choices.map((item) => (
+        <li style={{ color: "black", fontSize: "16px" }}>
+          <input
+            type="radio"
+            value={[item.id, item.votes]}
+            name="choice"
+            onChange={(e) => {
+              setRadio(e.target.value);
+            }}
+          />
+          {item.text}
+        </li>
+      ))}
+      {!choices.length && (
+        <h2 style={{ color: "black", fontSize: "10px" }}> ...loading poll</h2>
+      )}
 
-        fetch(`/api/choices/${radio[0]}/`, {
-            method: 'PATCH',
-            headers: {
-                'Accept':'application/json',
-                'Content-Type': 'application/json',
-            },
-            body:JSON.stringify(newData)
-        }).then((result) => {
-            result.json().then((resp) => {
-                console.log(resp)
-                window.location.replace(`/results/${pathID}`)
-            })
-    })
-    
-}
+      <br />
 
+      <button className="mybutton" type="button" onClick={() => updateVote()}>
+        Vote
+      </button>
 
+      <br />
 
-    return (
-    <div> 
-
-    {question.length > 0 ? (
-            question.map((item) => (
-                <li><h1 style={{color: "blue", fontSize: "18px"}}>{item.text}</h1>	</li>))
-        ) : (
-                <h2 style={{color: "black", fontSize: "10px"}}> ...loading questions</h2>
-        )}
-
-            {choices.length > 0 ? (choices.map((item) => (
-                    <li style={{color: "black", fontSize: "16px"}}> 
-                        <input 
-                            type="radio" 
-                            value={[item.id, item.votes]} 
-                            name="choice"
-                            //checked = {radio === ('item.id')}
-                            onChange={(e)=>{setRadio(e.target.value)}} />
-                        {item.text}</li>))
-                ) : (
-                    <h2 style={{color: "black", fontSize: "10px"}}> ...loading poll</h2>
-                )}
-
-        <br></br>
-            <button className = "mybutton"
-                type="button" 
-                onClick={() => updateVote()}>
-                    Vote
-                                      
-            </button>
-
-    <br></br>
-
-    <h1 style={{color: "blue", fontSize: "12px"}}><Link to={'/index'}>Back to polls</Link></h1>
-    </div>
-
-    
-    )};
+      <h1 style={{ color: "blue", fontSize: "12px" }}>
+        <Link to="/index">Back to polls</Link>
+      </h1>
+    </>
+  );
+};
 
 export default Detail;
--- front/src/components/Index.jsx
+++ front/src/components/Index.jsx
@@ -1,30 +1,35 @@
+import { Link } from "react-router-dom";
 import React from "react";
-import { Link } from 'react-router-dom';
-import useQuestions from "components/Questions"
-
+import useQuestions from "components/Questions";
+// Currently displays all questions in database. Not practical if there are a lot of questions. Need to update so
+// that it displays the five latest polls.
 const Index = () => {
+  const questions = useQuestions();
+  const listQuestion = (item) => {
+    const pollHeader = `-${item.id}:${item.text}`;
+    const publishDate = `Published: ${item.pubDate}`;
 
-	const questions = useQuestions() // new
+    return (
+      <li>
+        <h1 style={{ color: "black", fontSize: "16px" }}>
+          <Link to={`/detail/${item.id}`}>{pollHeader}</Link>
+        </h1>
+        <h2 style={{ color: "black", fontSize: "10px" }}>{publishDate}</h2>
+      </li>
+    );
+  };
+  const noQuestions = () => (
+    <h2 style={{ color: "black", fontSize: "10px" }}>...loading questions</h2>
+  );
 
-	return (
-		<div>
-			 <h1 style={{color: "blue", fontSize: "32px"}}>Polls</h1>
-			{questions.length > 0 ? (
-				questions.map((item) => (
-					<li>
-						<h1 style={{color: "black", fontSize: "16px"}}><Link to={'/detail/'+ item.id}>-{item.id}: {item.text}</Link></h1>
-            <h2 style={{color: "black", fontSize: "10px"}}>Published: {item.pubDate}</h2>
-					</li>
-				))
-			) : (
-        <h2 style={{color: "black", fontSize: "10px"}}> ...loading questions</h2>
-			)}
-		</div>
-    )
+  return (
+    <>
+      <h1 style={{ color: "blue", fontSize: "32px" }}>Polls</h1>
+      {questions.length > 0
+        ? questions.map((item) => listQuestion(item))
+        : noQuestions()}
+    </>
+  );
 };
 
-        {/* {
-          indexData.map(question => 
-            <h1 style={{color: "black", fontSize: "16px"}}><Link to={'/detail'}>- {question}</Link></h1>)
-        } */}
 export default Index;
