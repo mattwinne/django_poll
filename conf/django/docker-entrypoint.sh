8e02b5293039e9d5b71718db27888ac3231fa32b
Matthew Winne <93726628+mattwinne@users.noreply.github.com>
2022-03-12 15:14:51 -0800

    Djangointerface (#20)
    * Django WSGI functionality with Gunicorn and UWSGI, selectable via variable in .env file
    
    * Django WSGI functionality with Gunicorn and UWSGI, selectable via variable in .env file, linted
    
    * Added .env creating to github actions
    
    * Modified github actions
    
    * Modified github actions2
    
    * Modified github actions3
    
    * Modified github actions with secrets on .env file

--- conf/django/docker-entrypoint.sh
+++ conf/django/docker-entrypoint.sh
@@ -5,13 +5,28 @@ sleep 2s
 
 echo "Migrating the db..."
 python manage.py makemigrations
-python manage.py makemigrations polls
 python manage.py migrate
 
 if [ ${DEBUG:=0} -ne 1 ]
 then
   echo "Collecting static files into STATIC_ROOT..."
   python manage.py collectstatic --noinput
-fi
+  if [ ${APP_SERVER} == "wsgi" ]
+  then
+    echo "Launching uWSGI"
+    echo ${APP_SERVER}
+    uwsgi --http 0.0.0.0:8000 --module=mysite3.wsgi:application --pythonpath .. --env DJANGO_SETTINGS_MODULE=mysite3.settings
 
+  elif [ ${APP_SERVER} == "gunicorn" ]
+  then
+    echo "Launching gunicorn"
+    echo ${APP_SERVER}
+    gunicorn --workers=3 --threads=3 mysite3.wsgi -b 0.0.0.0:8000
+  else
+    echo "No app_server env variable launching uwsgi"
+    echo ${APP_SERVER}
+    uwsgi --http 0.0.0.0:8000 --module=mysite3.wsgi:application --pythonpath .. --env DJANGO_SETTINGS_MODULE=mysite3.settings
+  fi
+fi
 echo "Running any commands passed to this script from CMD of command..."
+exec "$@"
